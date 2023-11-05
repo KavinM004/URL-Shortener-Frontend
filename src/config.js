d4f9859bfb0e1a44054bcd0d14ad833610ecc2d3
karthick <karthickkavin004@gmail.com>
2023-11-05 14:51:01 +0530

    first commit

---
+++ src/config.js
@@ -0,0 +1,7 @@
+export const config = {
+    api: "https://url-shortener-backend-liart.vercel.app",
+    client: "https://shortening-url-app-frontend.netlify.app"
+}
+
+//for backend local    http://localhost:4000
+//for frontend local    http://localhost:3000
